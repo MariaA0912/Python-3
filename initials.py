d560ff9845584ded022aec0c3a8f84c3620da9e7
Maria Arvizu <143664674+MariaA0912@users.noreply.github.com>
2024-06-11 13:08:23 -0700

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,24 @@
+Initial = '''
+    SSS     L
+  S     S   L
+  S         L
+    SSS     L
+        S   L
+  S     S   L
+    SSS     LLLLL
+'''
+print(Initial)
+
+#Maria Arvizu
+#I like playing Call of Duty Black Ops Zombies
+
+firstInitial = '''
+    M          M          A
+    M  M    M  M        A   A 
+    M     M    M      A       A
+    M          M      A A A A A
+    M          M      A       A
+    M          M      A       A
+    M          M      A       A
+    '''
+print(firstInitial)
