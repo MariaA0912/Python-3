0cabab788bf0fc4c38e503c6bcb4750fd76d6965
Maria Arvizu <143664674+MariaA0912@users.noreply.github.com>
2024-06-26 16:02:45 -0700

    Create magic8.py

---
+++ magic8.py
@@ -0,0 +1,25 @@
+import random
+
+name = "Camila"
+question = "What is the meaning of life?"
+answer = ""
+
+random_number = random.randit(1,9)
+
+if random_number == 1:
+  answer = "Yes - definitely"
+
+elif random_number == 2:
+  answer = "It is decidedly so"
+
+elif random_number == 3:
+  answer = "Without a doubt"
+
+elif random_number == 4:
+  answer = "Reply hazy, try again"
+
+elif random_number == 5:
+  answer = "Ask again later" 
+
+elif random_number == 6:
+  answer = "Better not tell you now"
