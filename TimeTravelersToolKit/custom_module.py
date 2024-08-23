031564a8fcd76d0c015f0d08a206ea2c116c239c
Maria Arvizu <143664674+MariaA0912@users.noreply.github.com>
2024-08-23 00:33:53 -0700

    Create custom_module.py

---
+++ TimeTravelersToolKit/custom_module.py
@@ -0,0 +1,2 @@
+def generate_time_travel_message(year, destination, cost):
+  return "To travel to the year {year}, with a destination of {destination}. It will cost {cost}.".format(year = year, destination  = destination, cost = cost)
